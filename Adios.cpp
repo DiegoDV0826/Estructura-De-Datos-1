60587e64377d7ec67ae0af08a82675acd33a4e8c
Diego <A01747310@itesm.mx>
2019-06-20 14:48:51 -0500

    test

--- Adios.cpp
+++ Adios.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Adios" << std::endl;
     std::cout << "Ya me voy" << std ::endl;
     std::cout << "Tengo tarea" << std::endl;
+    std::cout << "2" << std::endl;
     return 0;
 }
